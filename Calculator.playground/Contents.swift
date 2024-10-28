a18f087a466906d2b6ab9744655698245fc577a6
DoyleHWorks <DoyleHWorks@gmail.com>
2024-10-28 16:35:17 +0900

    [INIT] Calculator.playground

---
+++ Calculator.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Personal Project for Learning Swift
+//
+// Created by DOYLE 2024-10-28 16:31
+//
+// Calculator
+
+class Calculator {
+        // Todo : 내부 구현하기
+}
+
+let calculator = Calculator() // 인스턴스 생성하여 변수에 할당
+
+// Todo : calculator 변수를 활용하여 사칙연산을 진행
