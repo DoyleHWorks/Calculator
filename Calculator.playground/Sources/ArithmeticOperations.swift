454ff9be80147cde390a3498c6d8b1f5f7f6b3ed
DoyleHWorks <DoyleHWorks@gmail.com>
2024-10-30 21:29:40 +0900

    [REFACTOR] protocol renamed: Operation -> AbstractOperation

--- Calculator.playground/Sources/ArithmeticOperations.swift
+++ Calculator.playground/Sources/ArithmeticOperations.swift
@@ -6,40 +6,40 @@
 //
 
 // Protocol for Operation Classes
-protocol Operation {
+protocol AbstractOperation {
     func operate(_ num1: Double, _ num2: Double) -> Double
 }
 
 // Arithmetic Operation Classes
-public class AddOperation: Operation {
+public class AddOperation: AbstractOperation {
     public init() {}
     public func operate(_ num1: Double, _ num2: Double) -> Double {
         return num1 + num2
     }
 }
 
-public class SubtractOperation: Operation {
+public class SubtractOperation: AbstractOperation {
     public init() {}
     public func operate(_ num1: Double, _ num2: Double) -> Double {
         return num1 - num2
     }
 }
 
-public class MultiplyOperation: Operation {
+public class MultiplyOperation: AbstractOperation {
     public init() {}
     public func operate(_ num1: Double, _ num2: Double) -> Double {
         return num1 * num2
     }
 }
 
-public class DivideOperation: Operation {
+public class DivideOperation: AbstractOperation {
     public init() {}
     public func operate(_ num1: Double, _ num2: Double) -> Double {
         return num1 / num2
     }
 }
 
-public class ModulusOperation: Operation {
+public class ModulusOperation: AbstractOperation {
     public init() {}
     public func operate(_ num1: Double, _ num2: Double) -> Double {
         return num1.truncatingRemainder(dividingBy: num2) // Since they are Double, cannot use '/'.
