558bdce18fcf6bb9b5e949389651f045d9fa7aea
DoyleHWorks <DoyleHWorks@gmail.com>
2024-10-30 22:54:24 +0900

    [REFACTOR] separated operation components

---
+++ Calculator.playground/Sources/Operations/AddOperation.swift
@@ -0,0 +1,13 @@
+//
+//  AddOperation.swift
+//  
+//
+//  Created by t0000-m0112 on 10/30/24.
+//
+
+public class AddOperation: AbstractOperation {
+    public init() {}
+    public func operate(_ num1: Double, _ num2: Double) -> Double {
+        return num1 + num2
+    }
+}
---
+++ Calculator.playground/Sources/Operations/MultiplyOperation.swift
@@ -0,0 +1,13 @@
+//
+//  MultiplyOperation.swift
+//  
+//
+//  Created by t0000-m0112 on 10/30/24.
+//
+
+public class MultiplyOperation: AbstractOperation {
+    public init() {}
+    public func operate(_ num1: Double, _ num2: Double) -> Double {
+        return num1 * num2
+    }
+}
