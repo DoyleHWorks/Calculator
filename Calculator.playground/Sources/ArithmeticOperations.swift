754488ef721948afa6832ed1950adcd77257a7d8
DoyleHWorks <DoyleHWorks@gmail.com>
2024-10-30 18:43:02 +0900

    [COMMENT] added explanatory comments

--- Calculator.playground/Sources/ArithmeticOperations.swift
+++ Calculator.playground/Sources/ArithmeticOperations.swift
@@ -5,10 +5,12 @@
 //  Created by DOYLE on 10/29/24.
 //
 
+// Protocol for Operation Classes
 protocol Operation {
     func operate(_ num1: Double, _ num2: Double) -> Double
 }
 
+// Arithmetic Operation Classes
 public class AddOperation: Operation {
     public init() {}
     public func operate(_ num1: Double, _ num2: Double) -> Double {
@@ -40,6 +42,6 @@ public class DivideOperation: Operation {
 public class ModulusOperation: Operation {
     public init() {}
     public func operate(_ num1: Double, _ num2: Double) -> Double {
-        return num1.truncatingRemainder(dividingBy: num2)
+        return num1.truncatingRemainder(dividingBy: num2) // Since they are Double, cannot use '/'.
     }
 }
